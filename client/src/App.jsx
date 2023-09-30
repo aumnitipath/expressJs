ead964e606555398a490e21616c575212729d9d2
aumnitipath <aumnitipath@outlook.com>
2023-10-01 00:24:53 +0700

    feat:style FormProduct

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,29 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FormProduct from "./components/FormProduct";
 import FormEditProduct from "./components/FormEditProduct";
+import HeaderBar from "./layout/HeaderBar";
+import { CssBaseline, Box } from "@mui/material";
+import SideBar from "./layout/SideBar";
 
 function App() {
   return (
     <BrowserRouter>
       <>
-        <h1>From CRUD</h1>
-        <Routes>
-          <Route path="/" element={<FormProduct />} />
-          <Route path="/edit/:id" element={<FormEditProduct />} />
-        </Routes>
+        <CssBaseline />
+        <div className="app">
+          <SideBar />
+          <main className="content">
+            <HeaderBar />
+            <div className="content_body">
+              <Box m="20px">
+                <Routes>
+                  <Route path="/admin/viewtable" element={<FormProduct />} />
+                  <Route path="/edit/:id" element={<FormEditProduct />} />
+                </Routes>
+              </Box>
+            </div>
+          </main>
+        </div>
       </>
     </BrowserRouter>
   );
--- client/src/components/FormProduct.jsx
+++ client/src/components/FormProduct.jsx
@@ -4,6 +4,19 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { create, getData, remove } from "../functions/product";
 
+// import Box from "@mui/material/Box";
+// import TextField from "@mui/material/TextField";
+import { Box, TextField, Button } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditTwoToneIcon from "@mui/icons-material/EditTwoTone";
+
 const FormProduct = () => {
   const [data, setData] = useState([]);
   const [form, setForm] = useState({});
@@ -61,66 +74,100 @@ const FormProduct = () => {
 
   return (
     <div>
-      FormProduct
+      <h2>Form Product</h2>
       <form onSubmit={handleSubmit} encType="multipart/form-data">
-        <input
-          type="text"
-          name="name"
-          onChange={(e) => handleChange(e)}
-          placeholder="name"
-        />
-        <br />
+        <div>
+          <TextField
+            id="outlined-basic"
+            label="name"
+            name="name"
+            onChange={(e) => handleChange(e)}
+            variant="outlined"
+          />
+        </div>
 
-        <input
-          type="text"
-          name="detail"
-          placeholder="detail"
-          onChange={(e) => handleChange(e)}
-        />
-        <br />
+        <div className="my-3">
+          <TextField
+            id="outlined-basic"
+            label="detail"
+            name="detail"
+            onChange={(e) => handleChange(e)}
+            variant="outlined"
+          />
+        </div>
 
-        <input type="file" name="file" onChange={(e) => handleChange(e)} />
-        <br />
+        <div className="my-3">
+          <TextField
+            type="file"
+            id="outlined-basic"
+            label="file"
+            name="file"
+            onChange={(e) => handleChange(e)}
+            variant="outlined"
+            focused
+          />
+        </div>
 
-        <input
-          type="text"
-          name="price"
-          placeholder="price"
-          onChange={(e) => handleChange(e)}
-        />
+        <div className="my-3">
+          <TextField
+            type="number"
+            id="outlined-basic"
+            label="price"
+            name="price"
+            onChange={(e) => handleChange(e)}
+            variant="outlined"
+          />
+        </div>
 
-        <button>Submit</button>
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
       </form>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">name</th>
-            <th scope="col">detail</th>
-            <th scope="col">file</th>
-            <th scope="col">price</th>
-            <th scope="col">action</th>
-            <th scope="col">edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data
-            ? data.map((item, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{item.name}</td>
-                  <td>{item.detail}</td>
-                  <td>{item.file}</td>
-                  <td>{item.price}</td>
-                  <td onClick={() => handleRemove(item._id)}>delete</td>
-                  <td>
-                    <Link to={"/edit/" + item._id}>edit</Link>
-                  </td>
-                </tr>
-              ))
-            : null}
-        </tbody>
-      </table>
+
+      <TableContainer component={Paper} className="my-6">
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell scope="col">No.</TableCell>
+              <TableCell scope="col">Name</TableCell>
+              <TableCell scope="col">Detail</TableCell>
+              <TableCell scope="col">File</TableCell>
+              <TableCell scope="col">Price</TableCell>
+              <TableCell scope="col">Delete</TableCell>
+              <TableCell scope="col">Edit</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {data
+              ? data.map((item, index) => (
+                  <TableRow
+                    key={index}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell>{item.name}</TableCell>
+                    <TableCell>{item.detail}</TableCell>
+                    <TableCell>{item.file}</TableCell>
+                    <TableCell>{item.price}</TableCell>
+                    <TableCell>
+                      <DeleteIcon
+                        className="cursor-pointer"
+                        color="error"
+                        onClick={() => handleRemove(item._id)}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Link to={"/edit/" + item._id}>
+                        <EditTwoToneIcon className="bg-blue-200" />
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
